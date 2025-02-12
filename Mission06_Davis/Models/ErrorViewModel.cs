7e236f870cd9b475c40729ab35570e0acfe1749a
Tasedit <tasedit@gmail.com>
2025-02-12 13:42:39 -0700

    Add project files.

---
+++ Mission06_Davis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_Davis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
